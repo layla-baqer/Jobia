59a96aaee357baa04cb80ec05edfcb901c623e98
FatimaAliAbdulla6248 <fatimaradhi99@gmail.com>
2022-11-17 13:05:09 +0300

    Fatima Last update

--- frontend/src/Components/NavBar/NavBar.jsx
+++ frontend/src/Components/NavBar/NavBar.jsx
@@ -1,36 +1,32 @@
 import React from 'react'
 import './NavBar.css'
 import { Link, useParams } from 'react-router-dom'
-// import FontAwesomeIcon from '@fortawesome/react-fontawesome'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 
 function NavBar(props) {
   return (
-
     <div className='navbar-container'>
-        <ul className="navbar-ul">
-          <div>
-            
-          <FontAwesomeIcon icon="fa-light fa-user-plus" size='large' />
-            <Link to='/user/home'>Home</Link>
-          </div>
-          <div>
-            <li><Link to={'/profile'}>Profile</Link></li>
-          </div>
-          <div>
-            <li><Link to='/Signup'>Sigup</Link></li>
-          </div>
-          <div>
-            <li><Link to='/Login'>Login</Link></li>
-          </div>
-          <div>
-            <li><Link to='/about'>About</Link></li>
-          </div>
-          <div>
-            <li><Link to='/logout' onClick={props.onLogoutHandler}>Logout</Link></li>
-          </div>
-        </ul>
+    {props.isAuth ? (
+        <div>
+          <ul className="navbar-ul">
+          <div><Link to='/user/home'>Home</Link></div>
+          <div><Link to={'/profile'}>Profile</Link></div>
+          <div><Link to='/about'>About</Link></div>
+
+          <div><Link to="/logout" onClick={props.onLogoutHandler}>Logout</Link></div>
+          {/* {props.user ? "Welcome, " + props.user.user.firstName : null} */}
+          </ul>
+        </div>
+     ) : (
+      <div>
+       <ul className="navbar-ul">
+        <div><Link to="/Signup">Signup</Link></div>
+        <div><Link to="/Login">Lognin</Link></div>
+       </ul>
+      </div>
+     )}
+
     </div>
   )
 }
--- frontend/src/Components/user/Signup.js
+++ frontend/src/Components/user/Signup.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react'
 import {Container, Form, Button} from "react-bootstrap";
+import s
 
 export default function Signup(props) {
 
